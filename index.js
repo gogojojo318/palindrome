36bb77b7449b39e7774bc35b17249a9cd83ff633
jojo <mikiyui@mikiyuinoMacBook-Air.local>
2023-10-29 13:30:36 +0900

    Initialize repository

--- index.js
+++ index.js
@@ -1,30 +1,20 @@
-module.exports = Phrase;
-
-// 文字列を逆順にする
-function reverse(string) {
-    return Array.from(string).reverse().join("");
+// reverse()メソッドを追加してすべてのStringで使えるようにする
+String.prototype.reverse = function() {
+    return Array.from(this).reverse().join("");
   }
   
   // Phraseオブジェクトを定義する
   function Phrase(content) {
     this.content = content;
   
-   // パリンドロームのテスト用に変換した訳文を返す
-   this.processedContent = function processedContent() {
-    return this.letters().toLowerCase();
+    // パリンドロームのテスト用に変換した訳文を返す
+    this.processedContent = function processedContent() {
+      return this.content.toLowerCase();
     }
   
-  // コンテンツの文字だけを返す
-  // 利用例:
-  //   new Phrase("Hello, world!").letters() === "Helloworld"
-  this.letters = function letters() {
-    return (this.content.match(/[a-z]/gi) || []).join("");
-  }
-
-
     // パリンドロームならtrueを、違うならfalseを返す
     this.palindrome = function palindrome() {
-      return this.processedContent() === reverse(this.processedContent());
+      return this.processedContent() === this.processedContent().reverse();
     }
   }
   
